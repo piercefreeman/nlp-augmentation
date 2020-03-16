561980f461c337339b42362fd038a641051cc602
Pierce Freeman <piercefreeman@gmail.com>
2020-03-16 16:53:55 -0700

    add gpu support

--- nlp_augmentation/backtranslation/backtranslate.py
+++ nlp_augmentation/backtranslation/backtranslate.py
@@ -12,19 +12,21 @@ from logging import info
 
 
 class BackTranslate(AugmentationBase):
-    def __init__(self, model_dir, scratch_dir, replicas=1, worker_id=0, sampling_temp=0.8):
+    def __init__(self, model_dir, scratch_dir, replicas=1, worker_id=0, sampling_temp=0.8, gpu_count=0):
         """
         :param replicas: An argument for parallel preprocessing. For example, when replicas=3,
             we divide the data into three parts, and only process one part
             according to the worker_id.
         :param sampling_temp: The sampling temperature for translation. See README.md for more
             details.
+        :param gpu: quantity of gpus to use
 
         """
         self.model_dir = str(model_dir)
         self.replicas = replicas
         self.worker_id = worker_id
         self.sampling_temp = sampling_temp
+        self.gpu_count = gpu_count
 
         scratch_dir = Path(scratch_dir)
         self.doc_len_dir = scratch_dir / "doc_len"
@@ -77,7 +79,8 @@ class BackTranslate(AugmentationBase):
                 "--output_dir=/tmp/t2t",
                 f"--decode_from_file={self.forward_src_dir}/file_{self.worker_id}_of_{self.replicas}.txt",
                 f"--decode_to_file={self.forward_gen_dir}/file_{self.worker_id}_of_{self.replicas}.txt",
-                f"--data_dir={self.model_dir}"
+                f"--data_dir={self.model_dir}",
+                f"--worker_gpu={self.gpu_count}",
             ]
         )
 
@@ -94,7 +97,8 @@ class BackTranslate(AugmentationBase):
                 "--output_dir=/tmp/t2t",
                 f"--decode_from_file={self.forward_gen_dir}/file_{self.worker_id}_of_{self.replicas}.txt",
                 f"--decode_to_file={self.backward_gen_dir}/file_{self.worker_id}_of_{self.replicas}.txt",
-                f"--data_dir={self.model_dir}"
+                f"--data_dir={self.model_dir}",
+                f"--worker_gpu={self.gpu_count}",
             ]
         )
 
